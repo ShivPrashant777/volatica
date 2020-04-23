d0058eeaa77a00b392c08620114a635f8530ec75
ShivPrashant777 <shivprashant777@gmail.com>
2020-04-23 23:05:36 +0530

    UI Update v2

--- public/js/client.js
+++ public/js/client.js
@@ -5,75 +5,77 @@ var userList = document.getElementById('users-list');
 var logo = document.getElementById('logo');
 var roomName = document.getElementById('room-name');
 var usersToggle = document.getElementById('users-toggle');
-var messages = document.getElementById('messages');  
+var messages = document.getElementById('messages');
 
 // Users Toggle for phones
 toggle = false;
-usersToggle.addEventListener('click', function(){
-    if (toggle == false){
-        users.style.display = "block";
-        toggle = true;
-    } else {
-        users.style.display = "none";
-        toggle = false;
-    }
+usersToggle.addEventListener('click', function () {
+	if (toggle == false) {
+		users.style.display = 'block';
+		toggle = true;
+	} else {
+		users.style.display = 'none';
+		toggle = false;
+	}
 });
 
-
-// Light Theme
-var light = false;
-logo.addEventListener("click", function(){
-    if (light == false) {
-        document.documentElement.setAttribute('data-theme', 'light');    
-        light = true;      
-    } else {
-        document.documentElement.setAttribute('data-theme', 'dark'); 
-        light = false;
-    }
+// Dark Theme
+var dark = false;
+logo.addEventListener('click', function () {
+	if (dark == false) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		dark = true;
+	} else {
+		document.documentElement.setAttribute('data-theme', 'light');
+		dark = false;
+	}
 });
 
-function autoScrollDown(){
-    msgContainer.scrollTop = msgContainer.scrollHeight;
+function autoScrollDown() {
+	msgContainer.scrollTop = msgContainer.scrollHeight;
 }
 
-function sendMessage(message){
-    const div = document.createElement('div');
-    div.classList.add('message');
-    div.innerHTML = `<p class="sender">${message.username} <span>${message.time}</span> </p>
+function sendMessage(message) {
+	const div = document.createElement('div');
+	div.classList.add('message');
+	div.innerHTML = `<p class="sender">${message.username} <span>${message.time}</span> </p>
     <p class="text">
         ${message.text}
     </p>`;
-    document.getElementById('msg-container').appendChild(div);
-    autoScrollDown();
-} 
+	if (message.username == 'You') {
+		div.classList.add('right');
+	} else if (message.username == 'Admin') {
+		div.classList.add('center');
+	} else {
+		div.classList.add('left');
+	}
+	document.getElementById('msg-container').appendChild(div);
+	autoScrollDown();
+}
 
 // Socket Functions
-socket.on('newUser', function(user){    
-    socket.emit('joinRoom', {username: user.username, room: user.room});
+socket.on('newUser', function (user) {
+	socket.emit('joinRoom', { username: user.username, room: user.room });
 
-    socket.on('getUsers', function(data){
-        roomName.innerHTML = data.room;
-        userList.innerHTML = `
-        ${data.users.map(person => `<li>${person.username}</li>`).join('')}`
-    });
+	socket.on('getUsers', function (data) {
+		roomName.innerHTML = data.room;
+		userList.innerHTML = `
+        ${data.users.map((person) => `<li>${person.username}</li>`).join('')}`;
+	});
 
-    $('form').submit(function(event){
-        event.preventDefault();
-        socket.emit('getMsg', {msg: $('#m').val(), sender: user.username, room: user.room});  // Send to server
-        $('#m').val('')
-        return false;
-    });
-})
-
-// Receive from server
-socket.on('sendMsg', function(data){
-    sendMessage(data);
+	$('form').submit(function (event) {
+		event.preventDefault();
+		socket.emit('getMsg', {
+			msg: $('#m').val(),
+			sender: user.username,
+			room: user.room,
+		}); // Send to server
+		$('#m').val('');
+		return false;
+	});
 });
 
-
-
-
-
-
-
-
+// Receive from server
+socket.on('sendMsg', function (data) {
+	sendMessage(data);
+});
